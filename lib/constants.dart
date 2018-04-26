f339d70bc24a6f2e242875227142c9ba50aa623f
Wahib-Ul-Haq <wahib.tech@gmail.com>
2018-04-26 11:27:54 +0200

    Updated open the door text

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 const String APP_TITLE = "Hodor : KI labs";
-const String SUCCESS_OPEN_DOOR_MSG = "Now, Press the Door!";
+const String SUCCESS_OPEN_DOOR_MSG = "Now, Open the Door!";
 const String FAILURE_OPEN_DOOR_MSG = "Network Error! Something went wrong";
 const String OPEN_DOOR_BUTTON_LABEL = "Open The Door";
 const String FAILURE_INTERNET_MSG = "Sorry, You Are Not Connected To Internet!";
